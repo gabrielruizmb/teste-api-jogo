c3b13709bba4b8067365ca89e6b88d9c1aa62aa6
gabrielruizmb <105463724+gabrielruizmb@users.noreply.github.com>
2022-06-06 13:19:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
-        'X-RapidAPI-Key': '756224436cmsh60d57eed64064e9p155bb6jsn5bbadf6424cb'
-    }
-};
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+    'X-RapidAPI-Key': '756224436cmsh60d57eed64064e9p155bb6jsn5bbadf6424cb'
+  }
+}
 
-let games = [];
-let platform, category;
-function changeCategory (newCategory) {
-    category = newCategory;
-    loadGames()
+let games = []
+let platform, category
+function changeCategory(newCategory) {
+  category = newCategory
+  loadGames()
 }
 
 function loadGames() {
-    let data1 = "";
+  let data1 = ''
 
-    games.filter(game => {
-        if (!category) return true;
-        if (game.genre.includes(category)) return true;
-
-    }).forEach((values) => {
-        data1 += `<div class="cartao1">
-           <img src="${values.thumbnail}" alt="imagem" class="img">
-           <h1 class="titulo">${values.title}</h1>
-           <p>Genero: ${values.genre}</p>
-           <p>Plataforma: ${values.platform}</p>
-       </div>`
+  games
+    .filter(game => {
+      if (!category) return true
+      if (game.genre.includes(category)) return true
+    })
+    .forEach(values => {
+      data1 += `<div class="cartao1">
+         <img src="${values.thumbnail}" alt="imagem" class="img">
+         <h1 class="titulo">${values.title}</h1>
+         <p>Genero: ${values.genre}</p>
+         <p>Plataforma: ${values.platform}</p>
+     </div>`
     })
 
-    /*const filteredData = ('button') => {
-            const filteredData = values.filter(values => values.genre === button)
-            setItemenu(filteredData)
-            
-            }*/
-    document.getElementById('cartao').innerHTML = data1;
+  /*const filteredData = ('button') => {
+          const filteredData = values.filter(values => values.genre === button)
+          setItemenu(filteredData)
+          
+          }*/
+  document.getElementById('cartao').innerHTML = data1
 }
 
 fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games`, options)
-    .then(response => response.json())
-    //.then(response => console.log(response))
-    /*.catch(err => console.error(err));*/
+  .then(response => response.json())
+  //.then(response => console.log(response))
+  /*.catch(err => console.error(err));*/
 
-    .then((completedata) => {
-        games = completedata;
-        loadGames()
-    }).catch((erro) => {
-        console.log(erro);
-    })
+  .then(completedata => {
+    games = completedata
+    loadGames()
+  })
+  .catch(erro => {
+    console.log(erro)
+  })
