50aaa0f1c6f5ce8cc286967a218b87cef786c91f
gabrielruizmb <105463724+gabrielruizmb@users.noreply.github.com>
2022-06-07 19:24:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
-        'X-RapidAPI-Key': '756224436cmsh60d57eed64064e9p155bb6jsn5bbadf6424cb'
-    }
-};
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+    'X-RapidAPI-Key': '756224436cmsh60d57eed64064e9p155bb6jsn5bbadf6424cb'
+  }
+}
+fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games`, options) //api do site
+  .then(response => response.json()) //arquivos para json
+  .then(completedata => {
+    games = completedata
+    loadGames() //funcao dos jogos
+  })
+  .catch(erro => {
+    //tratamento de erro
+    console.log(erro)
+  })
 
-let games = [];
-let platform, category;
-function changeCategory (newCategory) {
-    category = newCategory;
-    loadGames()
+let games = [] //recebe o filter
+let category
+let platform
+function changeCategory(newCategory) {
+  category = newCategory
+  loadGames()
+}
+function changeplatform(newplatform) {
+  platform = newplatform
+  loadGames()
 }
 
 function loadGames() {
-    let data1 = "";
-
-    games.filter(game => {
-        if (!category) return true;
-        if (game.genre.includes(category)) return true;
-
-    }).forEach((values) => {
-        data1 += `<div class="cartao1">
-           <img src="${values.thumbnail}" alt="imagem" class="img">
-           <h1 class="titulo">${values.title}</h1>
-           <p>Genero: ${values.genre}</p>
-           <p>Plataforma: ${values.platform}</p>
-       </div>`
+  //funcao faz aparecer os jogos na tela
+  let data1 = '' //variavel como string vazia
+  games
+    .filter(game => {
+      if (!category) return true
+      if (game.genre.includes(category)) {
+        if (game.genre.includes(category) && game.platform.includes(platform)) {
+          genre = platform
+        } else {
+          return game.genre
+        }
+      }
     })
-
-    /*const filteredData = ('button') => {
-            const filteredData = values.filter(values => values.genre === button)
-            setItemenu(filteredData)
-            
-            }*/
-    document.getElementById('cartao').innerHTML = data1;
+    .forEach(values => {
+      //roda um arrey retornando vazio
+      data1 += `<div class="cartao1"> <a target="blank" href="${values.freetogame_profile_url}"> 
+          <img src="${values.thumbnail}" alt="imagem" class="img">
+         <h1 class="titulo">${values.title}</h1>
+         <p>Genero: ${values.genre}</p>
+         <p>Plataforma: ${values.platform}</p>
+         </div></a>` 
+    })
+  document.getElementById('cartao').innerHTML = data1
 }
-
-fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games`, options)
-    .then(response => response.json())
-    //.then(response => console.log(response))
-    /*.catch(err => console.error(err));*/
-
-    .then((completedata) => {
-        games = completedata;
-        loadGames()
-    }).catch((erro) => {
-        console.log(erro);
-    })
